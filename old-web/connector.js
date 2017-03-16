6ab4d4ddb6afce7a8281a2ca4830f88f63c4b46d
Marcel Behrmann <behrmann@lfi.rwth-aachen.de>
2017-03-16 09:37:22 +0100

    Removed old Code

--- old-web/connector.js
+++
@@ -1,7 +0,0 @@
-class Connector {
-  
-  constructor() {
-    this.center = createVector();
-
-  }
-}
--- old-web/nodes/map.js
+++
@@ -1,5 +0,0 @@
-class MapNode extends Node {
-  constructor(x, y) {
-    super(x, y, 200, 100, "Map");
-  }
-}
